5b3f8e675088e0ac77b8b2becd415769934a3d49
Aktar Zaman <aktarzaman@berkeley.edu>
2019-09-08 14:12:29 -0700

    changed pub_date to publish date

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -14,14 +14,14 @@ def index(request):
 # Show specific question and choices
 def detail(request, question_id):
   try:
-    question = Question.objects.get(pk=question_id)
+    question = Question.objects.get(pk = question_id)
   except Question.DoesNotExist:
     raise Http404("Question does not exist")
   return render(request, 'polls/detail.html', { 'question': question })
 
 # Get question and display results
 def results(request, question_id):
-  question = get_object_or_404(Question, pk=question_id)
+  question = get_object_or_404(Question, pk = question_id)
   return render(request, 'polls/results.html', { 'question': question })
 
 # Vote for a question choice
